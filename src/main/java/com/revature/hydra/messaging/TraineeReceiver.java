7ace6186117cfe1400c065d4e9ef8f53626d9318
bzahler <maaraken@yahoo.com>
2018-03-28 15:42:23 -0400

    added comments to sender/receiver

--- src/main/java/com/revature/hydra/messaging/TraineeReceiver.java
+++ src/main/java/com/revature/hydra/messaging/TraineeReceiver.java
@@ -13,18 +13,41 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
+/**
+ * 
+ * @author Ben Zahler (Blake 1801)
+ *
+ * In order to receive messages through rabbitmq your computer must have port 5672 open.
+ * 
+ * This class is the first edition of utilizing rabbitmq messaging queues. Currently a queue is not 
+ * specified and instead rabbitmq creates it for us. This probably needs to change. Also the IP address
+ * is hard coded.
+ * 
+ * Currently (3/28/2018) this function is called from the trainee controller and is triggered by making
+ * a postman request to trainees/testing
+ *
+ */
 @Service
 public class TraineeReceiver {
 
+	// This was arbitrarily named but should be the same on both sender and receiver.
 	private static final String EXCHANGE_NAME = "hydra.trainee.exchange";
+	
 	
 	public void receive() throws IOException, TimeoutException {
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		
+		// This user was created on host machine through the rabbitmq management console (localhost:15672 as of 3/28/2018)
+		factory.setUsername("test");
+		factory.setPassword("test");
+		// Currently this is the hard coded address of the host.
+		factory.setHost("10.226.122.184");
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 		
+		// Fanout is important
 		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+		// I believe this needs to be changed so that messages aren't dropped.
 		String queueName = channel.queueDeclare().getQueue();
 		channel.queueBind(queueName, EXCHANGE_NAME, "");
 		
--- src/main/java/com/revature/hydra/messaging/TraineeSender.java
+++ src/main/java/com/revature/hydra/messaging/TraineeSender.java
@@ -9,21 +9,37 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+/**
+ * 
+ * @author Ben Zahler (Blake 1801)
+ *
+ * In order to receive messages through rabbitmq your computer must have port 5672 open.
+ * 
+ * This class is the first edition of utilizing rabbitmq messaging queues. Currently a queue is not 
+ * specified and instead rabbitmq creates it for us. This probably needs to change. Also the IP address
+ * is hard coded.
+ * 
+ * Currently (3/28/2018) this function is called from the trainee controller and is triggered by making
+ * a postman request to trainees/testing
+ *
+ */
 @Service 
 public class TraineeSender {
 
+	// This was arbitrarily named but should be the same on both sender and receiver.
 	private static final String EXCHANGE_NAME = "hydra.trainee.exchange";
 
 	public void send () throws IOException, TimeoutException {
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		// This user was created on host machine through the rabbitmq management console (localhost:15672 as of 3/28/2018)
+		factory.setUsername("test");
+		factory.setPassword("test");
+		factory.setHost("10.226.122.184");
+		// Currently this is the hard coded address of the host.
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
-		
 		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-		
 		String message = "WEEEEoooooWEEEEEooooWEEEoooo";
-		
 		channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
 		System.out.println(" [x] Sent '" + message + "'");
 		
